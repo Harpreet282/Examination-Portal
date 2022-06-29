01963d3a62e9a5da4fb9aa669974415f840f3b36
aditi803 <aditimehta300@gmail.com>
2022-06-29 12:13:29 -0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+import Profile from "./Pages/Profile-page/Profile";
+import Student from "./Pages/Student-Page/Student";
 import Index from "./Routes";
 
 function App() {
   return (
     <>
     
-    <Header />
+    {/* <Header />
     <Index />
-    <Footer />
+    <Footer /> */}
+    {/* <Profile /> */}
+    <Student />
     </>
   );
 }
---
+++ src/Pages/Student-Page/Student.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import "../Student-Page/Student.css"
+
+const Student = () => {
+  return (
+    <section>
+        <div className='student-page'>
+            <div className='container all-containers my-5'>
+                <div className='row'>
+                    <div className='col-md-6'>
+                        <div className='student-left-container'>
+                            <h1>Hello</h1>
+                        </div>
+                            
+
+                    </div>
+                    <div className='col-md-6'>
+                            <h1>Byeee</h1>
+                        </div>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Student
