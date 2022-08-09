0eb2235c7cc887c553ff072096bc18833481d010
aditi803 <aditimehta300@gmail.com>
2022-08-09 16:59:16 +0530

    gcxgf

--- src/Routes/Student-routes/index.js
+++ src/Routes/Student-routes/index.js
@@ -2,9 +2,11 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Exam from "../../Pages/Student-panel/studentView/Exams/Exams";
 import Results from "../../Pages/Student-panel/studentView/Results";
-import ExamGuildelines from "../../Pages/Student-panel/studentView/Exams/ExamGuildelines";
 import Transaction from "../../Pages/Student-panel/studentView/Transaction";
 // import Modal from "../../Pages/Student-panel/Timer/Modal";
+import KeyModal from '../../Modals/KeyModal'
+import ExamGuildelines from "../../Pages/Student-panel/studentView/Exams/ExamGuildelines"
+
 import Error from "../../Pages/Error";
 
 function Index() {
@@ -14,8 +16,10 @@ function Index() {
       <Route path="/" element={<Exam />} />
       <Route path="/results" element={<Results />} />
       <Route path="/transaction" element={<Transaction />} />
-      <Route path="/examguidelines" element={<ExamGuildelines />} />
+      {/* <Route path="/examguidelines" element={<ExamGuildelines />} /> */}
+      
       {/* <Route path="/modal" element={<Modal />} /> */}
+      <Route path='/keymodal' element={<KeyModal />} />
       <Route path="*" element={<Error />} />
     </Routes>
     </>
